372422aa5d572de476fb998b7bcd8b8180bf66d6
oguzhannx <oguzhanoztrk00@gmail.com>
2024-06-14 20:58:53 +0300

    he

--- Business/Abstract/IFirmalarService.cs
+++ Business/Abstract/IFirmalarService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using B2BApp.Entities.Concrete;
+using Core.Models.Concrete;
+using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,10 @@ namespace B2BApp.Business.Abstract
 {
     public interface IFirmalarService
     {
-        
+
+        void addFirma(Firma firma);
+        Result<ICollection<Firma>> getAll();
+        Result<Firma> getFirmaById(ObjectId objectId);
+
     }
 }
--- Business/Concrete/FirmalarService.cs
+++ Business/Concrete/FirmalarService.cs
@@ -1,4 +1,7 @@
 ﻿using B2BApp.DataAccess.Abstract;
+using B2BApp.Entities.Concrete;
+using Core.Models.Concrete;
+using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace B2BApp.Business.Abstract
 {
-    public class FirmalarService
+    public class FirmalarService : IFirmalarService
     {
         private readonly IUnitOfWork _unitOfWork;
 
@@ -16,7 +19,43 @@ namespace B2BApp.Business.Abstract
             _unitOfWork = unitOfWork;
         }
 
+        public void addFirma(Firma firma)
+        {
+            try
+            {
+                _unitOfWork.Firma.InsertOne(firma);
+            }
+            catch (Exception)
+            {
 
+                throw;
+            }
+        }
 
+        public Result<ICollection<Firma>> getAll()
+        {
+            try
+            {    
+                return _unitOfWork.Firma.GetAll();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Result<Firma> getFirmaById(ObjectId objectId)
+        {
+            try
+            {
+                return _unitOfWork.Firma.GetById(objectId.ToString());
+            }
+            catch (Exception)
+            {
+
+                throw ;
+            }
+            throw new NotImplementedException();
+        }
     }
 }
