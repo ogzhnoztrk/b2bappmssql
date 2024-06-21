6bb354d65cd14fb14359b25bd36b5a47d96935bb
oguzhannx <oguzhanoztrk00@gmail.com>
2024-06-21 11:09:58 +0300

    api service added

---
+++ B2BApp.Web/Services/IApiService.cs
@@ -0,0 +1,11 @@
+﻿namespace B2BApp.Web.Services
+{
+    public interface IApiService<T> 
+    {
+        Task<T> GetAsync(string endPoint);
+        Task<T> PostAsync(string endPoint, T data);
+        Task<T> PutAsync(string endPoint, T data);
+        Task DeleteAsync(string endPoint);
+     
+    }
+}
